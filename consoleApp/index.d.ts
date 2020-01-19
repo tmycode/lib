835d16f665b7a9918ebb74b4c5b6c13a7f816c70
jie feng <jiefeng@jie-2.local>
2020-01-19 18:23:17 +1030

    init commit

---
+++ consoleApp/index.d.ts
@@ -0,0 +1,6 @@
+export declare class consoleApp {
+    static async readPrompt(fieldDes: string): void;
+    static startWith(fn: any): void;
+    static close(): void;
+    static result(message: string): void;
+}
